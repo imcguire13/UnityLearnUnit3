b1d81727883a4fe3ff334fdff45e12bb93c9b29e
Ian McGuire <mcguirei@roverkids.org>
2025-12-17 13:50:37 -0500

    3.2 Complete

--- JumpForce/JumpForce/Assets/Scripts/SpawnManager.cs
+++ JumpForce/JumpForce/Assets/Scripts/SpawnManager.cs
@@ -6,9 +6,11 @@ public class SpawnManager : MonoBehaviour
     private Vector3 spawnPos = new Vector3(25, 0, 0);
     private float startDelay = 2;
     private float repeatRate = 2;
+    private PlayerController playerControllerScript;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
         InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
     }
 
@@ -19,6 +21,10 @@ public class SpawnManager : MonoBehaviour
     }
     void SpawnObstacle ()
     {
-        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        if(playerControllerScript.gameOver == false)
+        {
+            Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        }
+        
     }
 }
